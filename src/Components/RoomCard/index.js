7c1e6b22895209feec8151093d61869f285bc29f
Manisha <manchu24@gmail.com>
2022-10-17 23:20:24 +0530

    rooms

--- src/Components/RoomCard/index.js
+++ src/Components/RoomCard/index.js
@@ -1,24 +1,14 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { useState } from "react";
-import { useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function RecipeReviewCard() {
+function RoomCard() {
   const navigate = useNavigate();
   const [indexSelected, setIndexSelected] = useState(-1);
   const imageList = [
@@ -61,3 +51,5 @@ export default function RecipeReviewCard() {
     </Card>
   );
 }
+
+export default RoomCard;
