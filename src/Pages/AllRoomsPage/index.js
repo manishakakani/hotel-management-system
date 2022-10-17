c332e853a1bc3eff513573d9b74fdb8039d24dd4
Manisha <manchu24@gmail.com>
2022-10-17 12:48:22 +0530

    all rooms page

--- src/Pages/AllRoomsPage/index.js
+++ src/Pages/AllRoomsPage/index.js
@@ -1,14 +1,23 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import RoomCard from "../../Components/RoomCard";
 
 function AllRoomsPage() {
   return (
-    <Box display="flex" justifyContent="center" paddingTop={12}>
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      justifyContent="center"
+      alignItems={"center"}
+      paddingTop={12}
+    >
+      <Typography variant="h2" color="primary">
+        Rooms Available
+      </Typography>
       <Grid
         container
         spacing={4}
         justifyContent="flex-start"
-        marginTop={8}
+        marginTop={4}
         width="80%"
       >
         {[1, 2, 3, 4, 5, 6, 7, 8].map((a) => {
