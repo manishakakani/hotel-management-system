f2ac6e1674905513ae0e9aff6e2df44cc6d01547
Manisha <manchu24@gmail.com>
2022-10-17 14:54:35 +0530

    room details

---
+++ src/Pages/RoomDetails/index.js
@@ -0,0 +1,164 @@
+import { ChevronLeft, ChevronRight, Close, Done } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Icon,
+  IconButton,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+function RoomDetails() {
+  const [indexSelected, setIndexSelected] = useState(-1);
+  const imageList = [
+    "http://cdn.home-designing.com/wp-content/uploads/2015/03/chevron-wallpaper.jpg",
+    "http://cdn.home-designing.com/wp-content/uploads/2015/03/tufted-headboard.jpg",
+  ];
+  const lengthOfImageList = imageList.length;
+  const [imageToShow, setImageToShow] = useState("");
+
+  const changeImage = () => {
+    if (indexSelected === lengthOfImageList - 1) {
+      setImageToShow(imageList[0]);
+      setIndexSelected(0);
+    } else {
+      setImageToShow(imageList[indexSelected + 1]);
+      setIndexSelected(indexSelected + 1);
+    }
+  };
+
+  const handleLeftClick = () => {
+    if (indexSelected === 0) {
+      setImageToShow(imageList[lengthOfImageList - 1]);
+      setIndexSelected(lengthOfImageList - 1);
+    } else {
+      setImageToShow(imageList[indexSelected - 1]);
+      setIndexSelected(indexSelected - 1);
+    }
+  };
+
+  const handleRightClick = () => changeImage();
+
+  useEffect(() => {
+    if (imageList.length > 0) {
+      if (indexSelected == -1) {
+        setIndexSelected(0);
+        setImageToShow(imageList[0]);
+      } else {
+        setTimeout(() => {
+          changeImage();
+        }, 3000);
+      }
+    }
+  }, [indexSelected]);
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box
+        marginY={4}
+        display="flex"
+        flexWrap="nowrap"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <IconButton onClick={handleLeftClick}>
+          <ChevronLeft />
+        </IconButton>
+        <img
+          style={{ "max-width": "75%", "max-height": "400px" }}
+          src={imageToShow}
+          alt={imageToShow}
+        />
+        <IconButton onClick={handleRightClick}>
+          <ChevronRight />
+        </IconButton>
+      </Box>
+      <Box
+        width="50%"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="space-around"
+      >
+        <Box display="flex" justifyContent="flex-start">
+          <Typography variant="h6" color="primary" paddingRight={2}>
+            {" "}
+            Room No. :{" "}
+          </Typography>
+          <Typography variant="h6"> 201 </Typography>
+        </Box>
+        <Box display="flex" justifyContent="flex-start">
+          <Typography variant="h6" color="primary" paddingRight={2}>
+            {" "}
+            Room Type :{" "}
+          </Typography>
+          <Typography variant="h6"> Deluxe </Typography>
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        alignItems="center"
+        flexWrap="wrap"
+      >
+        <Typography variant="h6" color="primary" paddingRight={2}>
+          Cost/day :
+        </Typography>
+        <Typography variant="h6">$9.99</Typography>
+      </Box>
+      <Typography variant="caption">
+        (Check-in: 12 PM, Check-out: 11 AM)
+      </Typography>
+      <Box
+        display="flex"
+        width="50%"
+        justifyContent="space-around"
+        marginY={4}
+        flexWrap="wrap"
+      >
+        <Box display="flex" flexDirection="column" alignItems="flex-start">
+          <Typography variant="h6" color="primary" paddingRight={2}>
+            {" "}
+            Amenities :{" "}
+          </Typography>
+          <Typography variant="subtitle"> Free WiFi </Typography>
+          <Typography variant="subtitle"> Complementary Breakfast </Typography>
+          <Typography variant="subtitle"> Swimming Pool </Typography>
+        </Box>
+        <Box display="flex" flexDirection="column">
+          <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingX={4}
+          >
+            <Typography variant="title" color="green" display="flex">
+              <Done />
+            </Typography>
+            <Typography variant="title" paddingRight={2}>
+              Smoking
+            </Typography>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            paddingX={4}
+          >
+            <Typography variant="title" color="red" display="flex">
+              <Close />
+            </Typography>
+            <Typography variant="title" paddingRight={2}>
+              Pets Allowed
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Button variant="contained" color="primary" marginY={6}>
+        Check availability for booking{" "}
+      </Button>
+    </Box>
+  );
+}
+
+export default RoomDetails;
--- src/assets/routes/index.js
+++ src/assets/routes/index.js
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import AllRoomsPage from "../../Pages/AllRoomsPage";
 import LoginPage from "../../Pages/LoginPage";
+import RoomDetails from "../../Pages/RoomDetails";
 import SignUpPage from "../../Pages/SignupPage";
 import WelcomePage from "../../Pages/WelcomePage";
 
@@ -9,6 +10,7 @@ const AllRoutes = [
   <Route path="/login" exact element={<LoginPage />} />,
   <Route path="/signup" exact element={<SignUpPage />} />,
   <Route path="/rooms" exact element={<AllRoomsPage />} />,
+  <Route path="/room/:number" exact element={<RoomDetails />} />,
 ];
 
 export default AllRoutes;
